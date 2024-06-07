c09d8406f44011115563699d53295009667a4629
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2024-06-07 12:20:16 +0530

    added proper comments to route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,26 +1,27 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const scopes=[
-  "https://www.googleapis.com/auth/gmail.readonly",
-  "https://www.googleapis.com/auth/gmail.compose",
-  "https://www.googleapis.com/auth/gmail.modify",
-//   "https://www.googleapis.com/auth/gmail.metadata"
-]
-
-const handler =  NextAuth({
+// Create NextAuth handler
+const handler = NextAuth({
+  // Configure authentication providers
   providers: [
+    // Use GoogleProvider for authentication
     GoogleProvider({
+      // Use environment variables for client ID and client secret
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      // Specify additional authorization parameters
       authorization: {
         params: {
+          // Define the scope of access required for Google API
           scope: 'https://mail.google.com/ openid email profile',
         },
       },
     }),
   ],
+  // Configure callback functions for JWT and session
   callbacks: {
+    // Callback function for handling JWT
     async jwt({ token, account }) {
       // Persist the OAuth access token to the token right after signin
       if (account) {
@@ -29,6 +30,7 @@ const handler =  NextAuth({
       }
       return token;
     },
+    // Callback function for handling session
     async session({ session, token }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken;
@@ -39,5 +41,5 @@ const handler =  NextAuth({
   },
 });
 
-// const handler = NextAuth(authOptions);
+// Export NextAuth handler for both GET and POST requests
 module.exports = { GET: handler, POST: handler };
