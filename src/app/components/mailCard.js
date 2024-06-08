593ab89c09ea29413b4cae56516d15461bf874e4
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2024-06-08 16:53:37 +0530

    Updates in cards UI

--- src/app/components/mailCard.js
+++ src/app/components/mailCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './mailCard.css'
-function Card({ name, description }) {
+function Card({ name, description,type }) {
   return (
     <div className="card">
+      <div style={{display:'flex',justifyContent:'space-between'}}>
       <h2>{name}</h2>
+
+      {type && <h2 className={type.toLowerCase()}>{type}</h2>}
+      </div>
       <p>{description}</p>
     </div>
   );
